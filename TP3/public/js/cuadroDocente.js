a92f37ce6005501817e2c584036dd0a6bc32b331
asevey <marcoasevey15@gmail.com>
2021-09-15 10:54:11 -0300

    validacion de agregar y quitar docentes en fromcrearcurso

--- TP3/public/js/cuadroDocente.js
+++ TP3/public/js/cuadroDocente.js
@@ -21,11 +21,29 @@ object_select.addEventListener('click',()=>{
 
 function agregar(){
     let docente = document.createElement("option")
+    let docentes = Array.from(docentesEnd.options)
+    let bandera= 0;
     docente.setAttribute("value",docentesFrom.value)
     let docenteTexto = document.createTextNode(docentesFrom.value);
     docente.appendChild(docenteTexto);
-    docentesEnd.add(docente);
-}    
+
+    console.log(docentes)
+    if(docentes.length==0){
+        docentesEnd.add(docente);
+    }else {
+        for( let i=0; i<docentes.length; i++){
+            if(docentes[i].value == docente.value){
+                console.log("repetido")
+                bandera=1;
+            } 
+        }
+        if(bandera===0){
+            docentesEnd.add(docente);
+            bandera=0
+        }
+    }
+}
+
 function eliminar(){
     let opciones = Array.from(docentesEnd.selectedOptions)
     for(opcion of opciones){
