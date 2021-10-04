9de48ff2fa4dafb6a080f78fd3876f8ae1fc0c7c
Luis Gonzalo Morelli <42901084+luisgm21@users.noreply.github.com>
2021-10-03 21:40:12 -0300

    Correcciones y primeros modelos

--- src/apps/usuarios/models.py
+++ src/apps/usuarios/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    dni = models.IntegerField(unique=True)
+    nombreyapellido = models.CharField(max_length=100)
+    fecha_nacimiento= models.DateTimeField()
+    telefono= models.IntegerField()
+    sexo= models.CharField(max_length=15,null=False, blank=False)
+    domicilio=models.CharField(max_length=256,null=False, blank=False)
+class Usuario(models.Model):
+    username = models.CharField(max_length=15, unique=True,null=False,blank=False)
+    email= models.EmailField(unique=True)
+    password= models.TextField(null=False, blank=False)
+
--- src/proyectocursos/settings.py
+++ src/proyectocursos/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.cursos',
+    'apps.usuarios',
 ]
 
 MIDDLEWARE = [
@@ -62,8 +64,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'apps.cursos',
-                'apps.usuarios',
             ],
         },
     },
