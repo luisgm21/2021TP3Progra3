00c5b6a4154e8d72024902cd43c0a4daeebd402c
Luis Gonzalo Morelli <42901084+luisgm21@users.noreply.github.com>
2021-10-10 20:35:32 -0300

    Terminando Modelos Usuarios

--- src/apps/usuarios/models.py
+++ src/apps/usuarios/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
 class Persona(models.Model):
@@ -8,8 +9,17 @@ class Persona(models.Model):
     telefono= models.IntegerField()
     sexo= models.CharField(max_length=15,null=False, blank=False)
     domicilio=models.CharField(max_length=256,null=False, blank=False)
+    titulo=models.CharField(max_length=40,null=True)
+    class Meta:
+        abstract = True
 class Usuario(models.Model):
     username = models.CharField(max_length=15, unique=True,null=False,blank=False)
     email= models.EmailField(unique=True)
     password= models.TextField(null=False, blank=False)
+
+class Estudiante(Persona):
+    matricula= models.CharField(max_length=4)
+
+class Profesor(Persona):
+    fechaIngresoTrabajar= models.DateField(null=True)
 
