73bd01c5704f904088fda143514f750b479c10a9
asevey <marcoasevey15@gmail.com>
2021-10-11 19:37:55 -0300

    modelo de propuesta y curso

--- src/apps/cursos/models.py
+++ src/apps/cursos/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.db.models.base import Model
+from django.db.models.fields.mixins import FieldCacheMixin
+from apps.usuarios.models import Profesor
 
 # Create your models here.
+class curso(models.Model):
+    nombrecurso = models.CharField(max_length=100)
+    objetivogeneral = models.TextField(max_length=5000)
+    modalidad = models.CharField(max_length=10)
+    costo = models.IntegerField()
+    horascatedra = models.IntegerField()
+    cantidadminalumnos= models.IntegerField()
+    cantidadmaxalumnos=models.IntegerField()
+    fechaini=models.DateTimeField()
+    fechafin=models.DateTimeField()
+    docentecargo= models.ManyToManyField(Profesor)
+
+class propuesta(models.Model):
+    codcurso= models.OneToOneField(curso,on_delete=models.CASCADE)
+    descripcion= models.TextField(max_length=5000)
+    fecha= models.DateTimeField()
+    estado = models.CharField(max_length=30)
+    docente= models.OneToOneField(Profesor,on_delete=models.CASCADE)
+    
+    
