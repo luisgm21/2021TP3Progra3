e8e6b4997dd4480bc12c9f490f0d213a2b0217f4
asevey <marcoasevey15@gmail.com>
2021-10-19 19:19:13 -0300

    creacion de vistas y url datosCurso, estadisticas, estadoCursos ,inscripcion , pago

--- src/apps/cursos/url.py
+++ src/apps/cursos/url.py
@@ -1,10 +1,18 @@
 from django.conf.urls import url
 from django.urls import path
 from django.urls.resolvers import URLPattern
-from .views import tablaCursos
+from .views import tablaCursos,datosCurso,estadisticas,estadoCursos,inscripcion,pago
+
 app_name='cursos'
 
 urlpatterns=[
     #cursos views
-    path('', tablaCursos,name='registroCursos')
+    path('', tablaCursos,name='registroCursos'),
+    path('<int:pk>/', datosCurso,name='cursoDetalle'),
+    path('estadisticas/', estadisticas,name='estadisticas'),
+    path('estadocursos/', estadoCursos,name='estadoCursos'),
+    path('inscripcion/', inscripcion,name='inscrpcion'),
+    path('pago/', pago,name='pago'),
+
+
 ] 
--- src/apps/cursos/views.py
+++ src/apps/cursos/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 def tablaCursos(request):
     return render(request, 'cursos/tablaCursos.html')
+
+def datosCurso(request):
+    return render(request, 'cursos/datosCurso.html')   
+
+def estadisticas(request):
+    return render(request, 'cursos/estadisticas.html') 
+
+def estadoCursos(request):
+    return render(request, 'cursos/estadoCursos.html')   
+def inscripcion(request):
+    return render(request, 'cursos/inscripcion.html')   
+def pago(request):
+    return render(request, 'cursos/pago.html')   
