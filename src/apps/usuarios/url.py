e14d012646cbfb187750d2b69a94853b0fbb1f7d
Luis Gonzalo Morelli <42901084+luisgm21@users.noreply.github.com>
2021-10-31 20:01:57 -0300

    Implementando ABM Profesor, Estudiante

--- src/apps/usuarios/url.py
+++ src/apps/usuarios/url.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from django.urls import path
 from django.urls.resolvers import URLPattern
 
-from .views import listaEstudiantes, login,creacion_estudiante
+from .views import creacion_profesor, datosAlumno, datosProfesor, edicion_alumno, edicion_profesor, estudiante_delete, listaEstudiantes, listaProfesores, login,creacion_estudiante, profesor_delete
 
 app_name='usuarios'
 
@@ -10,6 +10,13 @@ urlpatterns=[
     #cursos views
     path('', login , name='login'),
     path('crear/estudiante',creacion_estudiante,name='crear_estudiante'),
-    path('listar/estudiante',listaEstudiantes,name="registro_usuario_estudiante")
-
+    path('listar/estudiante',listaEstudiantes,name="registro_usuario_estudiante"),
+    path('estudiante/<int:pk>/', datosAlumno ,name='alumnoDetalle'),
+    path('edicion/estudiante/<int:pk>',edicion_alumno,name='alumno_edicion'),
+    path('delete/estudiante',estudiante_delete, name='alumno_delete'),
+    path('crear/profesor',creacion_profesor,name='crear_profesor'),
+    path('listar/profesor',listaProfesores,name="registro_usuario_profesor"),
+    path('profesor/<int:pk>/', datosProfesor ,name='profesorDetalle'),
+    path('edicion/profesor/<int:pk>',edicion_profesor,name='profesor_edicion'),
+    path('delete/profesor',profesor_delete, name='profesor_delete'),
 ] 
--- src/apps/usuarios/views.py
+++ src/apps/usuarios/views.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 
-from apps.usuarios.models import Estudiante
+from apps.usuarios.models import Estudiante, Profesor
 from .forms import profesorForm,estudianteForm
 # Create your views here.
 
@@ -27,7 +27,7 @@ def creacion_estudiante(request):
 
 
 @csrf_exempt
-def edicion_curso(request, pk):
+def edicion_alumno(request, pk):
     estudiante = get_object_or_404(Estudiante, pk = pk)
 
     if (request.method == 'POST'):
@@ -35,24 +35,35 @@ def edicion_curso(request, pk):
         if estudiante_form.is_valid():
             estudiante_form.save(commit=True)
             messages.success(request,
-                             'Se ha editado correctamente el curso con el {}'.format(estudiante))
-            return redirect(reverse('cursos:registroCursos'))
+                             'Se ha editado correctamente el estudiante con el {}'.format(estudiante))
+            return redirect(reverse('usuarios:registro_usuario_estudiante'))
     else:
         estudiante_form = estudianteForm(instance=estudiante)    
-    return render(request,'cursos/modificarCurso.html',{'form': estudiante_form})
+    return render(request,'usuarios/modificarestudiante.html',{'form': estudiante_form})
 
 
 def estudiante_delete(request):
     if request.method == 'POST':
-        if 'id_estudiante' in request.POST:
-            estudiante = get_object_or_404(Estudiante, pk=request.POST['id_estudiante'])
+        if 'id_alumno' in request.POST:
+            estudiante = get_object_or_404(Estudiante, pk=request.POST['id_alumno'])
             nombre_estudiante = estudiante.nombreyapellido
             estudiante.delete()
-            messages.success(request, 'Se ha eliminado exitosamente el curso {}'.format(nombre_estudiante))
+            messages.success(request, 'Se ha eliminado exitosamente el estudiante {}'.format(nombre_estudiante))
         else:
-            messages.error(request, 'Debe indicar qué Programa se desea eliminar')
-    return redirect(reverse('usuarios:listaEstudiantes'))
+            messages.error(request, 'Debe indicar qué estudiante se desea eliminar')
+    return redirect(reverse('usuarios:registro_usuario_estudiante'))
 
+def listaEstudiantes(request):
+    return render(request,'usuarios/listaAlumnos.html',
+    {'alumnos': Estudiante.objects.all()})
+
+def datosAlumno(request,pk):
+    estudiante = get_object_or_404(Estudiante, pk=pk)
+    return render(request,
+                  'usuarios/alumnodetalle.html',
+                  {'alumno': estudiante})
+
+#--------------------------------------------------------------------------------------------------------------------------
 @csrf_exempt
 def creacion_profesor(request):
     if (request.method == 'POST'):
@@ -60,14 +71,48 @@ def creacion_profesor(request):
         if profesor_form.is_valid():
             nuevo_profesor = profesor_form.save(commit=True)
             messages.success(request,
-                             'Se ha agregado correctamente el Programa {}'.format(nuevo_profesor))
-            return redirect(reverse('cursos:registroCursos'))
+                             'Se ha agregado correctamente el profesor {}'.format(nuevo_profesor))
+            return redirect(reverse('usuarios:registro_usuario_profesor'))
     else:
         profesor_form = profesorForm()    
-    return render(request,'usuarios/inscripcion.html',{'form': profesor_form})
+    return render(request,'usuarios/crearprofesor.html',{'form': profesor_form})
+
+@csrf_exempt
+def edicion_profesor(request, pk):
+    profesor = get_object_or_404(Profesor, pk = pk)
+
+    if (request.method == 'POST'):
+        profesor_form = profesorForm(request.POST, request.FILES, instance=profesor)
+        if profesor_form.is_valid():
+            profesor_form.save(commit=True)
+            messages.success(request,
+                             'Se ha editado correctamente el profesor con el {}'.format(profesor))
+            return redirect(reverse('usuarios:registro_usuario_profesor'))
+    else:
+        profesor_form = profesorForm(instance=profesor)    
+    return render(request,'usuarios/modificarprofesor.html',{'form': profesor_form})
+
+
+def profesor_delete(request):
+    if request.method == 'POST':
+        if 'id_profesor' in request.POST:
+            profesor = get_object_or_404(Profesor, pk=request.POST['id_profesor'])
+            nombre_profesor = profesor.nombreyapellido
+            profesor.delete()
+            messages.success(request, 'Se ha eliminado exitosamente el profesor {}'.format(nombre_profesor))
+        else:
+            messages.error(request, 'Debe indicar qué profesor se desea eliminar')
+    return redirect(reverse('usuarios:registro_usuario_profesor'))
+
+def listaProfesores(request):
+    return render(request,'usuarios/listaProfesor.html',
+    {'profesores': Profesor.objects.all()})
+
+def datosProfesor(request,pk):
+    profesor = get_object_or_404(Profesor, pk=pk)
+    return render(request,
+                  'usuarios/profesordetalle.html',
+                  {'profesor': profesor})
 
 #----------------------------------------------------------------------------------------------------
-def listaEstudiantes(request):
-    return render(request,'usuarios/listaAlumnos.html',
-    {'alumnos': Estudiante.objects.all()})
 
